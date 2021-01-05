139ff2ac5439c7f6c02a2239bd36e9575b806873
PedroHamzehT <pedrohamzeh@gmail.com>
2021-01-05 16:57:52 -0300

    Update create probe migration

--- db/migrate/20210105195711_create_probes.rb
+++ db/migrate/20210105195711_create_probes.rb
@@ -1,9 +1,9 @@
 class CreateProbes < ActiveRecord::Migration[5.2]
   def change
     create_table :probes do |t|
-      t.integer :x_position
-      t.integer :y_position
-      t.integer :face
+      t.integer :x_position, default: 0
+      t.integer :y_position, default: 0
+      t.integer :face, default: 0
 
       t.timestamps
     end
