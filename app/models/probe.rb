3d8a8510e487b247b2846d51239f7364d4d346f9
PedroHamzehT <pedrohamzeh@gmail.com>
2021-01-05 17:07:50 -0300

    Create the probe methods to turn right or left and move

--- app/models/probe.rb
+++ app/models/probe.rb
@@ -1,2 +1,38 @@
 class Probe < ApplicationRecord
+  def current_direction
+    directions = %w[D B E C]
+
+    directions[face]
+  end
+
+  def turn_right
+    self.face += 1
+    self.face = 0 if face == 4
+
+    save!
+  end
+
+  def turn_left
+    self.face -= 1
+    self.face = -1 if face == -5
+
+    save!
+  end
+
+  def move
+    case current_direction
+    when 'D'
+      self.y_position += 1
+    when 'B'
+      self.x_position -= 1
+    when 'E'
+      self.y_position -= 1
+    when 'C'
+      self.x_position += 1
+    end
+
+    return false if (x_position.negative? || x_position > 4) || (y_position.negative? || y_position > 4)
+
+    save!
+  end
 end
