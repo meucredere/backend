d0b614d456992923a8175da89c06c82a9ad8f055
PedroHamzehT <pedrohamzeh@gmail.com>
2021-01-05 18:19:00 -0300

    Update routes to use probes in the url also

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      post '/move', to: 'probes#move'
-      get '/reset', to: 'probes#reset'
-      get '/current_position', to: 'probes#current_position'
+      post '/probes/move', to: 'probes#move'
+      get '/probes/reset', to: 'probes#reset'
+      get '/probes/current_position', to: 'probes#current_position'
     end
   end
 end
