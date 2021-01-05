50c52b0ac438cfa1007f06c912d0cb16972c0579
PedroHamzehT <pedrohamzeh@gmail.com>
2021-01-05 17:36:42 -0300

    Create the routes and put the controller inside the modules api and v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post '/move', to: 'probes#move'
+      get '/reset', to: 'probes#reset'
+      get '/current_position', to: 'probes#current_position'
+    end
+  end
 end
