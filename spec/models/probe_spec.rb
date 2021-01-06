2393d1e6b2852b76d3fdcaaf965c579abbc6bdaf
PedroHamzehT <pedrohamzeh@gmail.com>
2021-01-06 12:48:17 -0300

    Create the turn left test

--- spec/models/probe_spec.rb
+++ spec/models/probe_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 RSpec.describe Probe, type: :model do
   context 'Test turn right' do
     it 'should turn right the probe and return face equal B' do
-      probe = create(:probe)
+      probe = build(:probe)
       probe.turn_right
 
       expect(probe.current_direction).to eq('B')
+    end
+  end
+
+  context 'Test turn left' do
+    it 'should turn left the probe and return face equal C' do
+      probe = build(:probe)
+      probe.turn_left
+
+      expect(probe.current_direction).to eq('C')
     end
   end
 end
