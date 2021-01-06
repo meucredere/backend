d388cad0a44bafdfe6115ab8ce3a752c1d0f7a42
PedroHamzehT <pedrohamzeh@gmail.com>
2021-01-06 13:55:51 -0300

    Add numericality validation

--- app/models/probe.rb
+++ app/models/probe.rb
@@ -1,4 +1,7 @@
 class Probe < ApplicationRecord
+  validates :x_position, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 4 }
+  validates :y_position, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 4 }
+
   def current_direction
     directions = %w[D B E C]
 
